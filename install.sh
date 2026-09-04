bfb9740bad75b645d3e28eb2d4863eb759fa350a
Thijzert123 <184778919+Thijzert123@users.noreply.github.com>
2026-09-04 18:31:22 +0200

    Update

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "==> Installing pacman packages..."
+sudo pacman --noconfirm --needed -S \
+  adw-gtk-theme \
+  base-devel \
+  bash-completion \
+  bluez \
+  btop \
+  chezmoi \
+  ddcutil \
+  eza \
+  firefox \
+  foot \
+  fzf \
+  gdu \
+  git \
+  helix \
+  hyprland \
+  kdenlive \
+  less \
+  libreoffice-fresh \
+  networkmanager \
+  noctalia \
+  noto-fonts \
+  nwg-look \
+  openssh \
+  pipewire \
+  pipewire-jack \
+  power-profiles-daemon \
+  prismlauncher \
+  qt5-wayland \
+  qt6-multimedia-gstreamer \
+  qt6-wayland \
+  starship \
+  tree \
+  ttf-jetbrains-mono-nerd \
+  uwsm \
+  wireplumber \
+  xdg-desktop-portal-hyprland \
+  zoxide \
+
+echo "==> Enabling services..."
+sudo systemctl enable NetworkManager.service bluetooth.service
+
+echo "==> Installing yay..."
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si
+
+echo "==> Installing AUR packages"
+yay --noconfirm --needed -S \
+  qt6ct-kde
+
+echo "==> Setting GTK theme..."
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+
+echo "==> Cloning dotfiles..."
+chezmoi init --ssh Thijzert123
+
+echo "==> Installing hyprland plugins..."
+mkdir -p ~/.config/hypr/plugins
+cd ~/.config/hypr/plugins
+git clone https://github.com/zjeffer/split-monitor-workspaces
+cd split-monitor-workspaces
+# Use hyprland version here
+git fetch -Ppft && git checkout release/0.56.x
